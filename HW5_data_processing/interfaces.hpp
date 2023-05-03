c1701fb00d7349b377e63953348f72fef8ca585b
SmolinIV <Smolin1992@bk.ru>
2023-05-03 22:10:45 +0500

    HW5 uploaded

---
+++ HW5_data_processing/interfaces.hpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <limits>
+#include <math.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+
+//Интерфейс
+class IStatistics {
+public:
+	virtual ~IStatistics() {}
+
+	virtual void update(double next, const IStatistics* other = nullptr) = 0; //Параметр со значением по умолчанию нужен дочернему классу расчёта среднеквадратичного отклонения. см.ниже
+	virtual double value() const = 0;
+	virtual std::string name() const = 0;
+};
+
+class Min : public IStatistics{};
---
+++ HW5_data_processing/main.cpp
@@ -0,0 +1,68 @@
+#include "interfaces.hpp"
+#include "statistics.cpp"
+
+
+int main() {
+
+	enum Stats {
+		MIN,
+		MAX,
+		MEAN,
+		DEVIATION,
+		PERCENTILE90,
+		PERCENTILE95,
+		ALL_STATS
+	};
+
+	const size_t statistics_count = Stats::ALL_STATS;
+	IStatistics *statistics[statistics_count];
+
+	statistics[Stats::MIN] = new Min{};
+	statistics[Stats::MAX] = new Max{};
+	statistics[Stats::MEAN] = new Mean{};
+	statistics[Stats::DEVIATION] = new Std_deviation{};
+	statistics[Stats::PERCENTILE90] = new Percentile{90};
+	statistics[Stats::PERCENTILE95] = new Percentile{95};
+
+	double val = 0;
+	while (std::cin >> val) {
+		for (size_t i = 0; i < statistics_count; ++i) {
+			switch (i) {
+			case Stats::MIN:
+			case Stats::MAX:
+			case Stats::MEAN:
+				statistics[i]->update(val);
+				break;
+			case Stats::DEVIATION:
+				statistics[i]->update(val, statistics[i - 1]);
+				break;
+			case Stats::PERCENTILE90:
+				statistics[i]->update(val);
+				break;
+			case Stats::PERCENTILE95:
+				statistics[i]->update(val);
+				break;
+			}
+		}
+	}
+
+
+
+	// Handle invalid input data
+	if (!std::cin.eof() && !std::cin.good()) {
+		std::cerr << "Invalid input data\n";
+		return 1;
+	}
+
+	// Print results if any
+	for (size_t i = 0; i < statistics_count; ++i) {
+		std::cout << statistics[i]->name() << " = " << statistics[i]->value() << std::endl;
+	}
+
+	// Clear memory - delete all objects created by new
+	for (size_t i = 0; i < statistics_count; ++i) {
+		delete statistics[i];
+	}
+
+	return 0;
+}
