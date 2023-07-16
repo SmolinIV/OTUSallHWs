b43e7ac4313f9d23d54175ccf16aa7306c4caa23
SmolinIV <Smolin1992@bk.ru>
2023-07-16 23:07:33 +0500

    Continue doing hw8

--- HW8_thread/HW8_thread.cpp
+++ HW8_thread/HW8_thread.cpp
@@ -11,6 +11,9 @@
 #include <vector>
 #include <chrono>
 #include <fstream>
+#include <string>
+#include <thread>
+#include <sstream>
 
 const size_t TOPK = 4;
 
@@ -18,9 +21,9 @@ using Counter = std::map<std::string, std::size_t>;
 
 std::string tolower(const std::string &str);
 
-void count_words(std::istream& stream, Counter&);
+void count_words(std::vector<std::string>&, Counter&, int start_line, int end_line);
 
-void print_topk(std::ostream& stream, const Counter&, const size_t k);
+void print_topk(std::ostream& stream, const std::vector<Counter>&, const size_t k);
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -28,16 +31,35 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    auto start = std::chrono::high_resolution_clock::now();
-    Counter freq_dict;
-    for (int i = 1; i < argc; ++i) {
-        std::ifstream input{argv[i]};
-        if (!input.is_open()) {
-            std::cerr << "Failed to open file " << argv[i] << '\n';
-            return EXIT_FAILURE;
+	auto start = std::chrono::high_resolution_clock::now();
+	int num_threads = std::thread::hardware_concurrency();
+    Counter c_unused;
+    c_unused.emplace("", 0);
+	std::vector<Counter> freq_dict;
+	freq_dict.reserve(num_threads);
+	std::vector<std::string> text;
+	for (int i = 1; i < argc; ++i) {
+		std::ifstream input{argv[i]};
+		if (!input.is_open()) {
+			std::cerr << "Failed to open file " << argv[i] << '\n';
+			return EXIT_FAILURE;
+		}
+		std::string str_unused;
+		while (std::getline(input, str_unused)) { text.push_back(str_unused); }
+        int text_size = text.size();
+		int range = text_size / num_threads;
+		std::vector<std::thread> threads;
+		threads.reserve(num_threads);
+		for (int i = 0; i < num_threads; i++) {
+            freq_dict.push_back(c_unused);
+            std::thread counter_thread{count_words, std::ref(text), std::ref(freq_dict[i]), i*range, (i == num_threads - 1 ? text_size  : ((i + 1) * range) - 1)};
+            threads.push_back(std::move(counter_thread));
+		}
+
+        for (int i = 0; i < num_threads; i++) {
+            threads[i].join();
         }
-        count_words(input, freq_dict);
-    }
+	}
 
     print_topk(std::cout, freq_dict, TOPK);
     auto end = std::chrono::high_resolution_clock::now();
@@ -57,20 +79,29 @@ std::string tolower(const std::string &str) {
 
 
 
-void count_words(std::istream& stream, Counter& counter){
-    std::for_each(std::istream_iterator<std::string>(stream),
-                  std::istream_iterator<std::string>(),
-                  [&counter](const std::string &s) { ++counter[tolower(s)]; });    
+void count_words(std::vector<std::string>& text, Counter& counter, int start_line, int end_line){
+    for (auto i = text.begin() + start_line; i < text.begin() + end_line; i++) {
+        std::istringstream str_stream(*i);
+        std::for_each(std::istream_iterator<std::string>(str_stream),
+            std::istream_iterator<std::string>(),
+            [&counter](const std::string& s) { ++counter[tolower(s)]; });
+    }
 }
 
 
 
-void print_topk(std::ostream& stream, const Counter& counter, const size_t k) {
+void print_topk(std::ostream& stream, const std::vector<Counter>& counter, const size_t k) {
     std::vector<Counter::const_iterator> words;
-    words.reserve(counter.size());
-    for (auto it = std::cbegin(counter); it != std::cend(counter); ++it) {
-        words.push_back(it);
+    words.reserve(counter[0].size());
+    for (int i = 0; i < counter.size(); i++) {
+        for (auto it = std::cbegin(counter[i]); it != std::cend(counter[i]); ++it) {
+            for (int j = 0; j < words.size(); j++) {
+                if (words[j]->first == it->first) { words[j]->second += it->second; }
+
+            }
+        }
     }
+    words.shrink_to_fit();
 
     std::partial_sort(
         std::begin(words), std::begin(words) + k, std::end(words),
